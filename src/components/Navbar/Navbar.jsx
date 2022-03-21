6052ac9ec3c8dd1e17704d487542842c9e764f71
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-03-21 20:24:30 +0545

    mobile navigation menu for small device

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,43 @@
+import { useState } from 'react'
+
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {AiFillCloseCircle} from 'react-icons/ai'
 
 import logo from '../../assets/images/logo.png'
 import './Navbar.scss'
 
+const MenuItems = ['about', 'services', 'works', 'contact']
+
+const RenderMenuItems = () => (
+    MenuItems.map(item => (
+        <div key={item} className="app__navbar-links_item">
+            <a href={`#${item}`}>{item}</a>
+        </div>
+    ))
+)
+
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false)
   return (
     <div className="app__navbar">
         <div className="app__navbar-logo">
             <img src={logo} alt="logo" />
-            <p>Reena Maharjan</p>
+            <p style={{ color: showMenu ? '#fff' : '#913651' }}>Reena Maharjan</p>
+        </div>
+        <div className="app__navbar-links">
+            <RenderMenuItems />
         </div>
         <div className="app__navbar-menu">
-            <div className="app__navbar-menu_item">
-                <a href="#about">About</a>
-            </div>
-            <div className="app__navbar-menu_item">
-                <a href="#services">Services</a>
-            </div>
-            <div className="app__navbar-menu_item">
-                <a href="#works">Works</a>
-            </div>
-            <div className="app__navbar-menu_item">
-                <a href="#contact">Contact</a>
-            </div>
+            <GiHamburgerMenu  className="app__navbar-menu_hamburger" onClick={() => setShowMenu(true)} />
+            {showMenu && (
+                <div className="app__navbar-menu__overlay slide-bottom">
+                    <AiFillCloseCircle className="app__navbar-menu_close" onClick={() => setShowMenu(false)}/>
+                    <div className="app__navbar-links">
+                        <RenderMenuItems />
+                    </div>
+                </div>
+            )}
         </div>
-        {/* <div className="app__navbar-mobile">
-            <div className="app__navbar-menu_item">
-                <a href="#about">About</a>
-            </div>
-            <div className="app__navbar-menu_item">
-                <a href="#services">Services</a>
-            </div>
-            <div className="app__navbar-menu_item">
-                <a href="#works">Works</a>
-            </div>
-            <div className="app__navbar-menu_item">
-                <a href="#contact">Contact</a>
-            </div>
-        </div> */}
     </div>
   )
 }
